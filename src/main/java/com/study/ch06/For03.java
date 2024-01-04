b77ff65789fd16f2e30282b50f5657cb9b24b1e9
elikese <elikese@gamil.com>
2024-01-04 11:36:25 +0900

    continue / 별찍기 풀이

--- src/main/java/com/study/ch06/For03.java
+++ src/main/java/com/study/ch06/For03.java
@@ -6,15 +6,96 @@ public class For03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("별 몇개?  ");
-        int starCount = sc.nextInt();
+        System.out.print("별 몇줄? : ");
+        int starLine = sc.nextInt();
 
-        for (int i = 0; i < starCount; i++) {
-            for (int j = 0; j < i - 1; j++) {
-                int tmp = i - 1 - 1 - j;
-                
-            }
+        /*
+        for문 연습 별찍기
+         */
+
+        // *
+        // **
+        // ***
+        for (int i = 0; i < starLine; i++) {
             for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------");
+
+        // ***
+        // **
+        // *
+        for (int i = 0; i < starLine; i++) {
+            for (int j = 0; j < starLine - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------");
+
+        //  *
+        // **
+        //***
+        for (int i = 0; i < starLine; i++) {
+            for (int j = 0; j < starLine - (1 + i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (1 + i); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------");
+
+        //***
+        // **
+        //  *
+        for (int i = 0; i < starLine; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < starLine - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------");
+
+        //  *
+        // ***
+        //*****
+        for (int i = 0; i < starLine; i++) {
+            for (int j = 0; j < starLine - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------");
+
+
+        // 다이아몬드
+        // 위쪽
+        for (int i = 0; i < starLine; i++) {
+            for (int j = 0; j < starLine - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래쪽
+        for (int i = starLine - 2; i >= 0; i--) {
+            for (int j = 0; j < starLine - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
